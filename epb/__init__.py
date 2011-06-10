8b01f9f7e2fca0759f2d329efb74b7cbe81a780e
Michael Maltese <mike@mikemaltese.com>
2011-06-10 15:42:15 -0700

    Moved Blaster to epb/blast.py

--- epb/__init__.py
+++ epb/__init__.py
@@ -1,40 +1,12 @@
-from Bio.Blast import NCBIXML
 from collections import namedtuple
 import jinja2
 from os import path
-from StringIO import StringIO
-import subprocess
-from subprocess import PIPE
 
+from epb.blast import *
 from epb.fasta import *
 from epb.utils import *
 
 EPB_MODULEDIR = path.dirname(__file__)
-
-class BlastError(Exception):
-	def __init__(self, value): self.value = value
-	def __str__(self): return self.value
-
-class Blaster:
-	@classmethod
-	def blast(klass, **kwargs):
-		db = require_kw(kwargs, 'db')
-		seq = require_kw(kwargs, 'seq')
-		
-		p = subprocess.Popen(
-			("blastall -p blastp -d %s -F F -m7 -e 1e-5" % db).split(),
-			stdin=PIPE, stdout=PIPE, stderr=PIPE)
-		try:
-			p.stdin.write(seq + "\n")
-			out, err = p.communicate()
-			if p.returncode != 0:
-				raise BlastError, "return code of %i when blasting %s\n%s" % (p.returncode, db, err.strip())
-			xml = StringIO(out)
-			for record in NCBIXML.parse(xml):
-				yield record
-		except:
-			if p.returncode == None: p.kill()
-			raise
 
 class TaxonFileParser:
 	@classmethod
---
+++ epb/blast.py
@@ -0,0 +1,29 @@
+from Bio.Blast import NCBIXML
+from StringIO import StringIO
+import subprocess
+from subprocess import PIPE
+
+class BlastError(Exception):
+	def __init__(self, value): self.value = value
+	def __str__(self): return self.value
+
+class Blaster:
+	@classmethod
+	def blast(klass, **kwargs):
+		db = require_kw(kwargs, 'db')
+		seq = require_kw(kwargs, 'seq')
+		
+		p = subprocess.Popen(
+			("blastall -p blastp -d %s -F F -m7 -e 1e-5" % db).split(),
+			stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		try:
+			p.stdin.write(seq + "\n")
+			out, err = p.communicate()
+			if p.returncode != 0:
+				raise BlastError, "return code of %i when blasting %s\n%s" % (p.returncode, db, err.strip())
+			xml = StringIO(out)
+			for record in NCBIXML.parse(xml):
+				yield record
+		except:
+			if p.returncode == None: p.kill()
+			raise

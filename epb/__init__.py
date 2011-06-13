0177ad2390579d121464a3b5d5106d2acb55c8c0
Michael Maltese <mike@mikemaltese.com>
2011-06-13 08:26:52 -0700

    Cleaning up __init__

--- epb/__init__.py
+++ epb/__init__.py
@@ -1,4 +1,5 @@
 import jinja2
+import logging
 from os import path
 
 from epb.blast import *
@@ -7,34 +8,54 @@ from epb.organism import *
 from epb.taxon import *
 from epb.utils import *
 
-EPB_MODULEDIR = path.dirname(__file__)
+MODULEDIR = path.dirname(__file__)
 
-class DatabaseCollection:
-	def __init__(self, dir):
-		self.dir = dir
-	def find(self, name):
-		return path.join(self.dir, name)
+class EPB:
+	@classmethod
+	def organism_names(klass, taxon):
+		t = TaxonFileCollection(MODULEDIR)
+		return (OrganismName(*k) for k in t.parse(taxon))
+	
+	@classmethod
+	def blast(klass, seq, db_dir, names):
+		organisms = []
+		for name in names:
+			try:
+				matches = []
+				for record in Blaster.blast(db=path.join(db_dir, organism_name.database), seq=seq):
+					for a in record.alignments:
+						for hsp in a.hsps:
+							matches.append(OrganismMatch(a.title, seq, hsp))
+				organisms.append(Organism(name, matches))
+			except BlastError as e:
+				print "[Blaster] Error: %s" % e
+		return organisms
+	
+	@classmethod
+	def render(klass, organisms):
+		env = jinja2.Environment(loader = jinja2.PackageLoader('epb', 'templates'))
+		template = env.get_template('results.html.jinja2')
+		
+		return template.render(organisms = organisms)
+		
+	@classmethod
+	def go(klass, seq, taxon, db_dir):
+		log = logging.getLogger("epb.go")
+		log.debug("seq=%s" % seq)
+		log.debug("taxon=%s" % taxon)
+		
+		seq = Fasta.normalize(seq)
+		names = klass.organism_names(taxon)
+		results = klass.blast(seq, db_bir, names)
+		return klass.render(results)
 
 import sys
 if __file__ == sys.argv[0]:
-	taxon = sys.argv.pop()
-	seq = Fasta.normalize(sys.stdin.read())
+	logging.basicConfig(format = "%(asctime)s %(name)s: %(message)s", level=logging.DEBUG)
 	
-	db = DatabaseCollection("databases")
-	organisms = []
-	for organism_name in OrganismName.find_all_by_taxonomy(taxon):
-		try:
-			matches = []
-			for record in Blaster.blast(db=db.find(organism_name.database), seq=seq):
-				for a in record.alignments:
-					for hsp in a.hsps:
-						matches.append(OrganismMatch(a.title, seq, hsp))
-			organisms.append(Organism(organism_name, matches))
-		except BlastError as e:
-			print "[Blaster] Error: %s" % e
-	
-	env = jinja2.Environment(loader = jinja2.PackageLoader('epb', 'templates'))
-	template = env.get_template('results.html.jinja2')
-	
-	print template.render(organisms = organisms)
+	taxon = sys.argv[1]
+	seq = sys.stdin.read()
+
+	db_dir = "databases"
+	print EPB.go(seq, taxon, db_dir)
 	

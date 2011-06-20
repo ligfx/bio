5439f74e6e2981070ba9de05a30e05132d821bfb
Michael Maltese <mike@mikemaltese.com>
2011-06-19 23:34:16 -0700

    epb.Controller, switched to using Presenters

--- epb/__init__.py
+++ epb/__init__.py
@@ -12,6 +12,65 @@ from epb.taxon import *
 
 MODULEDIR = path.dirname(__file__)
 
+class Controller:
+	def __init__(self, params={}):
+		self.params = params
+		self.dbdir = self.params['dbdir']
+		self.logger = logging.getLogger("epb[Controller]")
+
+	def log(self, msg):
+		self.logger.debug(msg)
+		
+	def results(self):
+		# We've got three different pathways here
+		# 1) We have one sequence. Easy, just blast it and render it
+		# 2) We have multiple sequences, concatenated. Blast the concatenated
+		#    sequence, but give the renderer each individual sequence (so we can
+		#    see the boundaries)
+		# 3) We have multiple sequences, separate. Blast the sequences, and for each
+		#    record add an offset (erm? I have qualms about this), then render them like before
+		
+		names = self.params["names"]
+		fasta = self.params["sequence"]
+		method = self.params["method"] # 'concat' || 'multiple'
+		
+		if method == 'concat':
+			seq = Fasta.normalize(fasta)
+		elif method == 'multiple':
+			seq = fasta
+		else:
+			raise Exception, "method must be one of 'concat' or 'multiple'"
+		
+		organisms = []
+		for name in names:
+			self.log("blasting %s" % name.database)
+			try:
+				records = Blaster.blast(seq=seq, db=path.join(self.dbdir, name.database))
+				organisms.append(OrganismPresenter.from_name_and_records(name, records))
+			except BlastError as e:
+				logging.getLogger("blastall").warning("Error: %s" % e)
+		
+		sequences = list(Fasta.each(fasta))
+		total = len(seq)
+		p = None
+		for s in sequences:
+			s.width = "%f%%" % (s.size * 100.0 / total)
+		
+		return self.render("results", {
+			"organisms": organisms,
+			"sequences": sequences
+		})
+		
+	def render(self, action, context):
+		env = jinja2.Environment(loader = jinja2.PackageLoader('epb', 'templates'))
+		env.filters['as_percent'] = lambda value, total: "{0}%".format(value * 100.0 / total)
+		
+		template = env.get_template("%s.html.jinja2" % action)
+
+		context['total_sequence_percent_width'] = "%f%%" % sum(float(s.width.replace('%', '')) for s in context['sequences'][:-1])
+
+		return template.render(context)
+
 class EPB:
 	def __init__(self):
 		logging.basicConfig(format = "%(asctime)s %(name)s: %(message)s", level=logging.DEBUG)
@@ -19,58 +78,31 @@ class EPB:
 		
 		configfile = path.join(MODULEDIR, 'config.yaml')
 		self.log.debug("configuration: %s" % configfile)
-		with open(configfile) as f:
-			self.config = yaml.load(f)
+		with open(configfile) as f: self.config = yaml.load(f)
 
 		self.dbdir = self.config['dbdir']
 		self.log.debug("config['dbdir']: %s" % self.dbdir)
+		
+		self.taxons = TaxonFileCollection(MODULEDIR)
+		self.log.debug("taxons: %s" % self.taxons)
 	
-	def organism_names(self, taxon):
-		t = TaxonFileCollection(MODULEDIR)
-		return (OrganismName(*k) for k in t.parse(taxon))
-	
-	def blast(self, seq, names, cb=None):
-		organisms = []
-		for name in names:
-			if cb: cb()
-			self.log.debug("blasting %s" % name.database)
-			try:
-				o = Organism.blast(name=name, db=path.join(self.dbdir, name.database), seq=seq)
-				organisms.append(o)
-			except BlastError as e:
-				logging.getLogger("blastall").warning("Error: %s" % e)
-		return organisms
-	
-	def render(self, sequences, organisms):
-		env = jinja2.Environment(loader = jinja2.PackageLoader('epb', 'templates'))
-		template = env.get_template('results.html.jinja2')
-		
-		total_sequence_width = "%f%%" % sum(float(s.width.replace('%', '')) for s in sequences[:-1])
-		return template.render(organisms = organisms, sequences = sequences, total_sequence_width=total_sequence_width)
-		
-	def go(self, seq, taxon, cb=None):
-		self.log.debug("seq: %s" % seq)
-		self.log.debug("taxon: %s" % taxon)
-		
-		nseq = Fasta.normalize(seq)
-		self.log.debug("normalized sequence: %s" % nseq)
-		
-		names = self.organism_names(taxon)
-		results = self.blast(nseq, names, cb)
-		
-		seqs = list(Fasta.each(seq))
-		total = len(nseq)
-		p = None
-		for s in seqs:
-			s.width = "%f%%" % (s.size * 100.0 / total)
-		
-		return self.render(seqs, results)
+	def dispatch(self, params={}):
+		taxon = params['taxon']
+		params['names'] = [OrganismName(*k) for k in self.taxons.parse(params['taxon'])]
+		params['dbdir'] = self.dbdir
+		c = Controller(params)
+		return c.results()
 
 import sys
 if __file__ == sys.argv[0]:	
 	taxon = sys.argv[1]
+	method = sys.argv[2]
 	seq = sys.stdin.read()
 	
 	e = EPB()
-	print e.go(seq, taxon)
+	print e.dispatch({
+		"sequence": seq,
+		"method": method,
+		"taxon":taxon
+	})
 	

5c6b4df431e726e1a7ead740aacafadf5f472e98
Michael Maltese <mike@mikemaltese.com>
2011-06-27 14:57:36 -0700

    Don't need require_kw anymore

--- epb/utils.py
+++
@@ -1,7 +0,0 @@
-import inspect
-
-def require_kw(kwargs, key):
-	try:
-		return kwargs[key]
-	except KeyError:
-		raise TypeError("%s() requires '%s' keyword argument" % (inspect.stack()[1][3], key))

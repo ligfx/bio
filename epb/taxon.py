3106e35e891d275b37eb13c7ac82c2814a26ad6c
Michael Maltese <mike@mikemaltese.com>
2011-06-27 15:10:55 -0700

    Refactored taxon.py

--- epb/taxon.py
+++ epb/taxon.py
@@ -1,27 +1,15 @@
 from os import path
 
-class TaxonFileParser:
-	@classmethod
-	def parse(klass, stream):
-		for line in stream:
-			yield line.strip().split("\t", 2)
-
-class TaxonFileError(Exception):
-	def __init__(self, value): self.value = value
-	def __str__(self): return self.value
+class TaxonException(Exception): pass
 
 class TaxonFileCollection:
 	def __init__(self, dir):
 		self.dir = dir
 	
-	def find(self, name):
-		expanded = path.join(self.dir, "Taxon_%s.txt" % name)
-		if path.exists(expanded):
-			return expanded
-		else:
-			raise TaxonFileError("couldn't find Taxon_%s.txt in %s" % (name, self.dir))
-	
 	def parse(self, name):
-		with open(self.find(name)) as f:
-			for t in TaxonFileParser.parse(f):
-				yield t
+		fname = path.join(self.dir, "Taxon_%s.txt" % name)
+		if not path.exists(fname):
+			raise TaxonException, "Couldn't find 'Taxon_%s.txt'" % name
+		with open(fname) as f:
+			for line in f:
+				yield line.strip().split("\t", 2)
